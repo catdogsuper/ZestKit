0bfd35088c6b187e02346b6830c4737bc7efd916
raobinlin <raobinlin@icloud.com>
2024-08-26 11:22:57 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZestKit",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZestKit",
+            targets: ["ZestKit"]),
+        .library(
+            name: "ZestKitViews",
+            targets: ["ZestKitViews"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ZestKit",
+            dependencies: [
+                "ZestKitViews",
+            ]),
+        .binaryTarget(
+            name: "ZestKitViews",
+            path: "BinaryTargets/ZestKitViews.xcframework"),
+        .testTarget(
+            name: "ZestKitTests",
+            dependencies: ["ZestKit"]),
+    ]
+)
